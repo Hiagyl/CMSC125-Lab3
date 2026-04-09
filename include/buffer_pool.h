f715302f84dd4aa5bd6d59ef313050adb09fc593
Joshua Ticot <joshuaticot@gmail.com>
2026-04-09 21:42:10 +0800

    feat: added buffer pool boiler template

---
+++ include/buffer_pool.h
@@ -0,0 +1,19 @@
+#ifndef BUFFER_POOL
+#define BUFFER_POOL
+
+#define BUFFER_POOL_SIZE 5
+
+typedef struct {
+    int account_id;
+    Account* data;
+    bool in_use;
+} BufferSlot;
+
+typedef struct {
+    BufferSlot slots[BUFFER_POOL_SIZE];
+    sem_t empty_slots;
+    sem_t full_slots;
+    pthread_mutex_t pool_lock;
+} BufferPool;
+
+#endif
--- src/bank.c
+++ src/bank.c
@@ -19,6 +19,7 @@ void deposit(int account_id, int amount_centavos) {
     pthread_rwlock_unlock(&acc->lock);
 }
 
+
 bool withdraw(int account_id, int amount_centavos) {
     Account* acc = &bank.accounts[account_id];
     pthread_rwlock_wrlock(&acc->lock);
@@ -30,5 +31,33 @@ bool withdraw(int account_id, int amount_centavos) {
     
     acc->balance_centavos -= amount_centavos;
     pthread_rwlock_unlock(&acc->lock);
+    return true;
+}
+
+bool transfer(int from_id, int to_id, int amount_centavos) {
+    // Acquire locks in consistent order to prevent deadlock
+    int first = (from_id < to_id) ? from_id : to_id;
+    int second = (from_id < to_id) ? to_id : from_id;
+    
+    Account* acc_first = &bank.accounts[first];
+    Account* acc_second = &bank.accounts[second];
+    
+    pthread_rwlock_wrlock(&acc_first->lock);
+    pthread_rwlock_wrlock(&acc_second->lock);
+    
+    // Check sufficient funds
+    Account* from_acc = &bank.accounts[from_id];
+    if (from_acc->balance_centavos < amount_centavos) {
+        pthread_rwlock_unlock(&acc_second->lock);
+        pthread_rwlock_unlock(&acc_first->lock);
+        return false;
+    }
+    
+    // Perform transfer
+    bank.accounts[from_id].balance_centavos -= amount_centavos;
+    bank.accounts[to_id].balance_centavos += amount_centavos;
+    
+    pthread_rwlock_unlock(&acc_second->lock);
+    pthread_rwlock_unlock(&acc_first->lock);
     return true;
 }
