6370a0c9a0af513662288a1a90b58db243bc2e54
Joshua Ticot <joshuaticot@gmail.com>
2026-04-16 08:43:26 +0800

    feat: include stdbool.h in bank.h

--- include/bank.h
+++ include/bank.h
@@ -2,6 +2,7 @@
 #define BANK_H
 
 #include <pthread.h>
+#include <stdbool.h>
 
 #define MAX_ACCOUNTS 100
 
@@ -20,5 +21,6 @@ typedef struct {
 int get_balance(int account_id);
 void deposit(int account_id, int amount_centavos);
 bool withdraw(int account_id, int amount_centavos);
+bool transfer(int from_id, int to_id, int amount_centavos);
 
 #endif
